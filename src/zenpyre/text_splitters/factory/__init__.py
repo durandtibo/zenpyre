8be64c7b0e49d071b5526a67031d6e54736ace15
Thibaut Durand <durand.tibo@gmail.com>
2026-06-28 13:12:52 -0700

    Add `ConfigurableTextSplitterFactory` (#50)

--- src/zenpyre/text_splitters/factory/__init__.py
+++ src/zenpyre/text_splitters/factory/__init__.py
@@ -2,7 +2,8 @@ r"""Contains factory functions for text splitters."""
 
 from __future__ import annotations
 
-__all__ = ["BaseTextSplitterFactory", "TextSplitterFactory"]
+__all__ = ["BaseTextSplitterFactory", "ConfigurableTextSplitterFactory", "TextSplitterFactory"]
 
 from zenpyre.text_splitters.factory.base import BaseTextSplitterFactory
+from zenpyre.text_splitters.factory.configurable import ConfigurableTextSplitterFactory
 from zenpyre.text_splitters.factory.vanilla import TextSplitterFactory
