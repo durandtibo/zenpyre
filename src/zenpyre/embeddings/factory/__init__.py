3b1893c8e0097dcc329465cc5f1e27c2818f27e3
Thibaut Durand <durand.tibo@gmail.com>
2026-06-28 00:20:26 -0700

    Add `EmbeddingsFactory`

--- src/zenpyre/embeddings/factory/__init__.py
+++ src/zenpyre/embeddings/factory/__init__.py
@@ -2,6 +2,7 @@ r"""Contain embedding factories."""
 
 from __future__ import annotations
 
-__all__ = ["BaseEmbeddingsFactory"]
+__all__ = ["BaseEmbeddingsFactory", "EmbeddingsFactory"]
 
 from zenpyre.embeddings.factory.base import BaseEmbeddingsFactory
+from zenpyre.embeddings.factory.vanilla import EmbeddingsFactory
---
+++ tests/unit/embeddings/factory/test_vanilla.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+from langchain_core.embeddings.fake import FakeEmbeddings
+
+from zenpyre.embeddings.factory import BaseEmbeddingsFactory, EmbeddingsFactory
+
+
+def _make_embeddings() -> FakeEmbeddings:
+    """Return a FakeEmbeddings instance for testing."""
+    return FakeEmbeddings(size=128)
+
+
+###########################################
+#     Tests for EmbeddingsFactory         #
+###########################################
+
+
+def test_embeddings_factory_is_base_embeddings_factory() -> None:
+    assert isinstance(EmbeddingsFactory(_make_embeddings()), BaseEmbeddingsFactory)
+
+
+def test_embeddings_factory_make_embeddings_returns_embeddings() -> None:
+    embeddings = _make_embeddings()
+    assert EmbeddingsFactory(embeddings).make_embeddings() is embeddings
+
+
+def test_embeddings_factory_make_embeddings_returns_same_instance() -> None:
+    embeddings = _make_embeddings()
+    factory = EmbeddingsFactory(embeddings)
+    assert factory.make_embeddings() is factory.make_embeddings()
+
+
+def test_embeddings_factory_make_embeddings_returns_none() -> None:
+    assert EmbeddingsFactory(_make_embeddings()).make_embeddings() is not None
+
+
+def test_embeddings_factory_stores_embeddings() -> None:
+    embeddings = _make_embeddings()
+    assert EmbeddingsFactory(embeddings).embeddings is embeddings
+
+
+def test_embeddings_factory_different_instances_independent() -> None:
+    embeddings_a = _make_embeddings()
+    embeddings_b = _make_embeddings()
+    factory_a = EmbeddingsFactory(embeddings_a)
+    factory_b = EmbeddingsFactory(embeddings_b)
+    assert factory_a.make_embeddings() is not factory_b.make_embeddings()
