6fa2d5c4cb2e5b3bd784fa549d0c21ca791fa2cf
Thibaut Durand <durand.tibo@gmail.com>
2026-06-28 18:04:33 -0700

    Add more tests (#67)

--- tests/integration/embeddings/chroma/test_inspection.py
+++ tests/integration/embeddings/chroma/test_inspection.py
@@ -34,6 +34,13 @@ def vector_store() -> Chroma:
     )
 
 
+@pytest.fixture(scope="module")
+def empty_vector_store() -> Chroma:
+    """Return a real in-memory Chroma vector store populated with cat
+    documents."""
+    return Chroma(embedding_function=FakeEmbeddings(size=6))
+
+
 ###########################################
 #     Tests for inspect_embeddings        #
 ###########################################
@@ -52,3 +59,8 @@ def test_inspect_embeddings_chroma_custom_n(vector_store: Chroma) -> None:
 @langchain_chroma_available
 def test_inspect_embeddings_chroma_n_larger_than_embeddings(vector_store: Chroma) -> None:
     inspect_embeddings(vector_store, n=100)
+
+
+@langchain_chroma_available
+def test_inspect_embeddings_empty_vector_store_does_not_raise(empty_vector_store: Chroma) -> None:
+    inspect_embeddings(empty_vector_store)
