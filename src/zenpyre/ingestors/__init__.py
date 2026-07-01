b1f234c8fbcd4b4be530796585cfe38728fe986f
Thibaut Durand <durand.tibo@gmail.com>
2026-07-01 12:19:34 -0700

    Add `DataclassIngestor`

--- src/zenpyre/ingestors/__init__.py
+++ src/zenpyre/ingestors/__init__.py
@@ -2,8 +2,9 @@ r"""Contain all ingestors."""
 
 from __future__ import annotations
 
-__all__ = ["BaseIngestor", "PickleIngestor", "TextIngestor"]
+__all__ = ["BaseIngestor", "DataclassIngestor", "PickleIngestor", "TextIngestor"]
 
 from zenpyre.ingestors.base import BaseIngestor
+from zenpyre.ingestors.dataclass import DataclassIngestor
 from zenpyre.ingestors.pickle import PickleIngestor
 from zenpyre.ingestors.text import TextIngestor
