7776c89ded96993e048909fbc95d534cb40587b9
Thibaut Durand <durand.tibo@gmail.com>
2026-07-01 08:24:46 -0700

    Add `hash_documents` (#88)

--- src/zenpyre/documents/__init__.py
+++ src/zenpyre/documents/__init__.py
@@ -2,6 +2,6 @@ r"""Contain utilities for documents."""
 
 from __future__ import annotations
 
-__all__ = ["hash_document", "hash_document_uuid"]
+__all__ = ["hash_document", "hash_document_uuid", "hash_documents"]
 
-from zenpyre.documents.hashing import hash_document, hash_document_uuid
+from zenpyre.documents.hashing import hash_document, hash_document_uuid, hash_documents
