3b1dac9d3a796523d0e2da032e682e99677cce55
Thibaut Durand <durand.tibo@gmail.com>
2026-07-01 09:41:11 -0700

    Add `filter_by_*` (#92)

--- src/zenpyre/records/__init__.py
+++ src/zenpyre/records/__init__.py
@@ -2,7 +2,18 @@ r"""Define records."""
 
 from __future__ import annotations
 
-__all__ = ["Record", "sort_by_metadata"]
+__all__ = [
+    "Record",
+    "filter_by_metadata",
+    "filter_by_metadata_range",
+    "filter_by_metadata_values",
+    "sort_by_metadata",
+]
 
+from zenpyre.records.filter import (
+    filter_by_metadata,
+    filter_by_metadata_range,
+    filter_by_metadata_values,
+)
 from zenpyre.records.record import Record
 from zenpyre.records.sort import sort_by_metadata
