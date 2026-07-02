f1a48f84785dcf1298635156d3d1b778932319ca
Thibaut Durand <durand.tibo@gmail.com>
2026-07-01 18:21:53 -0700

    Add `LambdaProcessor` (#110)

--- src/zenpyre/data_processors/__init__.py
+++ src/zenpyre/data_processors/__init__.py
@@ -2,6 +2,7 @@ r"""Contain data processors."""
 
 from __future__ import annotations
 
-__all__ = ["BaseProcessor"]
+__all__ = ["BaseProcessor", "LambdaProcessor"]
 
 from zenpyre.data_processors.base import BaseProcessor
+from zenpyre.data_processors.lambdaa import LambdaProcessor
