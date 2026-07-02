c6b73e9c082befa7841369fd1dddb122f85e7785
Thibaut Durand <durand.tibo@gmail.com>
2026-07-01 18:32:20 -0700

    Add `SequenceProcessor`

--- src/zenpyre/data_processors/__init__.py
+++ src/zenpyre/data_processors/__init__.py
@@ -2,7 +2,8 @@ r"""Contain data processors."""
 
 from __future__ import annotations
 
-__all__ = ["BaseProcessor", "LambdaProcessor"]
+__all__ = ["BaseProcessor", "LambdaProcessor", "SequenceProcessor"]
 
 from zenpyre.data_processors.base import BaseProcessor
 from zenpyre.data_processors.lambdaa import LambdaProcessor
+from zenpyre.data_processors.sequence import SequenceProcessor
