474a97042b17a4985b3b353d7c002167d3ed6bf3
Thibaut Durand <durand.tibo@gmail.com>
2026-07-02 21:31:31 -0700

    Add `DocumentStoreLoader` (#136)

--- src/zenpyre/document_loaders/__init__.py
+++ src/zenpyre/document_loaders/__init__.py
@@ -2,7 +2,8 @@ r"""Contain document loaders."""
 
 from __future__ import annotations
 
-__all__ = ["DocumentListLoader", "resolve_document_loader"]
+__all__ = ["DocumentListLoader", "DocumentStoreLoader", "resolve_document_loader"]
 
+from zenpyre.document_loaders.document_store import DocumentStoreLoader
 from zenpyre.document_loaders.resolve import resolve_document_loader
 from zenpyre.document_loaders.sequence import DocumentListLoader
