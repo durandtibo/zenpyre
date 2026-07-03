9e53733df14aae5691837f95228f084364ecbd73
Thibaut Durand <durand.tibo@gmail.com>
2026-07-03 06:58:33 -0700

    Add `copy_ids_to_metadata` (#139)

--- src/zenpyre/documents/__init__.py
+++ src/zenpyre/documents/__init__.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 __all__ = [
     "assign_ids",
+    "copy_ids_to_metadata",
     "filter_by_metadata",
     "filter_by_metadata_range",
     "filter_by_metadata_values",
@@ -19,5 +20,5 @@ from zenpyre.documents.filter import (
     filter_by_metadata_values,
 )
 from zenpyre.documents.hashing import hash_document, hash_document_uuid, hash_documents
-from zenpyre.documents.id import assign_ids
+from zenpyre.documents.id import assign_ids, copy_ids_to_metadata
 from zenpyre.documents.sort import sort_by_metadata
