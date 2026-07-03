8997e9e8c26f1b352736e5567efd1fe1ceebeddc
Thibaut Durand <durand.tibo@gmail.com>
2026-07-03 01:02:54 -0700

    Add `print_document`

--- src/zenpyre/utils/rich/__init__.py
+++ src/zenpyre/utils/rich/__init__.py
@@ -7,12 +7,14 @@ __all__ = [
     "get_console",
     "make_progressbar",
     "make_spinner",
+    "print_document",
     "print_markdown",
     "print_pretty",
     "set_console",
 ]
 
 from zenpyre.utils.rich.console import get_console, set_console
+from zenpyre.utils.rich.document import print_document
 from zenpyre.utils.rich.logging import configure_rich_logging
 from zenpyre.utils.rich.print_ import print_markdown, print_pretty
 from zenpyre.utils.rich.progressbar import make_progressbar, make_spinner
