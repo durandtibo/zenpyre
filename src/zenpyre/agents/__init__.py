eb80edd7099b91698c19ffd1bf057c0d7d71dc5e
Thibaut Durand <durand.tibo@gmail.com>
2026-07-05 14:19:25 -0700

    Add `AgentConfig`

--- src/zenpyre/agents/__init__.py
+++ src/zenpyre/agents/__init__.py
@@ -2,6 +2,7 @@ r"""Contain agents."""
 
 from __future__ import annotations
 
-__all__ = ["RunnableWithCache"]
+__all__ = ["AgentConfig", "RunnableWithCache"]
 
 from zenpyre.agents.cache import RunnableWithCache
+from zenpyre.agents.config import AgentConfig
