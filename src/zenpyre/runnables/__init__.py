ef242e8db86f203de09c9fd491a86b8a9fa21f3c
Thibaut Durand <durand.tibo@gmail.com>
2026-07-07 13:40:42 -0700

    Refactor code

--- src/zenpyre/runnables/__init__.py
+++ src/zenpyre/runnables/__init__.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 __all__ = ["CachingRunnable", "InputOutputPair", "InputOutputRunnable"]
 
+from zenpyre.runnables import hashing as _hashing  # noqa: F401
 from zenpyre.runnables.cache import CachingRunnable
 from zenpyre.runnables.input_output import InputOutputRunnable
 from zenpyre.runnables.utils import InputOutputPair
