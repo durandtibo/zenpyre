047865246b22fb0abeb83d0b5615e2c28147f4f1
Thibaut Durand <durand.tibo@gmail.com>
2026-07-07 10:54:52 -0700

    Add `CachingRunnable`

--- src/zenpyre/runnables/__init__.py
+++ src/zenpyre/runnables/__init__.py
@@ -2,6 +2,8 @@ r"""Contain runnables."""
 
 from __future__ import annotations
 
-__all__ = ["InputOutputPair", "InputOutputRunnable"]
+__all__ = ["CachingRunnable", "InputOutputPair", "InputOutputRunnable"]
 
-from zenpyre.runnables.input_output import InputOutputPair, InputOutputRunnable
+from zenpyre.runnables.cache import CachingRunnable
+from zenpyre.runnables.input_output import InputOutputRunnable
+from zenpyre.runnables.utils import InputOutputPair
