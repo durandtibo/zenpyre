c3b0da9fee38a874bea49aefb6978a254f40c64b
Thibaut Durand <durand.tibo@gmail.com>
2026-07-07 10:30:32 -0700

    Add `InputOutputRunnable` (#175)

---
+++ src/zenpyre/runnables/__init__.py
@@ -0,0 +1,7 @@
+r"""Contain runnables."""
+
+from __future__ import annotations
+
+__all__ = ["InputOutputPair", "InputOutputRunnable"]
+
+from zenpyre.runnables.input_output import InputOutputPair, InputOutputRunnable
