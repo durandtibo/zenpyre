13d1bb33e853e62b57b358d547d56f70d6183956
Thibaut Durand <durand.tibo@gmail.com>
2026-07-08 13:30:06 -0700

    Add `DuckDBRecordStore`

--- src/zenpyre/record_stores/__init__.py
+++ src/zenpyre/record_stores/__init__.py
@@ -2,7 +2,8 @@ r"""Contain record stores."""
 
 from __future__ import annotations
 
-__all__ = ["BaseRecordStore", "InMemoryRecordStore"]
+__all__ = ["BaseRecordStore", "DuckDBRecordStore", "InMemoryRecordStore"]
 
 from zenpyre.record_stores.base import BaseRecordStore
+from zenpyre.record_stores.duckdb import DuckDBRecordStore
 from zenpyre.record_stores.in_memory import InMemoryRecordStore
