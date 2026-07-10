99a40ce67a57754eceae2c361d9132916ddd7820
Thibaut Durand <durand.tibo@gmail.com>
2026-07-09 18:21:18 -0700

    Add `ChatModelConfig`

---
+++ src/zenpyre/chat_models/__init__.py
@@ -0,0 +1,7 @@
+r"""Contain chat models."""
+
+from __future__ import annotations
+
+__all__ = ["BaseChatModelConfig", "ChatModelConfig"]
+
+from zenpyre.chat_models.config import BaseChatModelConfig, ChatModelConfig
