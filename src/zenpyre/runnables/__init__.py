06bbdf8b26350dc560bef66963c2be5f53f7cdf6
Thibaut Durand <durand.tibo@gmail.com>
2026-07-10 01:23:32 -0700

    Add `RecordingRunnable` (#216)

--- src/zenpyre/runnables/__init__.py
+++ src/zenpyre/runnables/__init__.py
@@ -6,11 +6,13 @@ __all__ = [
     "CachingRunnable",
     "InputOutputPair",
     "InputOutputRunnable",
+    "RecordingRunnable",
     "structured_output_runnable",
 ]
 
 from zenpyre.runnables import hashing as _hashing  # noqa: F401
 from zenpyre.runnables.cache import CachingRunnable
 from zenpyre.runnables.input_output import InputOutputRunnable
+from zenpyre.runnables.recording import RecordingRunnable
 from zenpyre.runnables.structured import structured_output_runnable
 from zenpyre.runnables.utils import InputOutputPair
