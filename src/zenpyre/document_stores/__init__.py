d6d62a4497b0cfa20acea7ef3883ed2d0b96b5c7
Thibaut Durand <durand.tibo@gmail.com>
2026-07-12 16:23:17 -0700

    Add `resolve_document_store` (#275)

--- src/zenpyre/document_stores/__init__.py
+++ src/zenpyre/document_stores/__init__.py
@@ -9,11 +9,13 @@ __all__ = [
     "SQLiteDocumentStore",
     "TypedDuckDBDocumentStore",
     "TypedSQLiteDocumentStore",
+    "resolve_document_store",
 ]
 
 from zenpyre.document_stores.base import BaseDocumentStore
 from zenpyre.document_stores.duckdb import DuckDBDocumentStore
 from zenpyre.document_stores.in_memory import InMemoryDocumentStore
+from zenpyre.document_stores.resolve import resolve_document_store
 from zenpyre.document_stores.sqlite import SQLiteDocumentStore
 from zenpyre.document_stores.typed_duckdb import TypedDuckDBDocumentStore
 from zenpyre.document_stores.typed_sqlite import TypedSQLiteDocumentStore
