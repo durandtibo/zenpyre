490234c11a7d3167f07e3dbbfa39acd85636776a
Thibaut Durand <durand.tibo@gmail.com>
2026-07-12 16:31:42 -0700

    Add `BaseDocumentStoreFactory`

---
+++ src/zenpyre/document_stores/factory/__init__.py
@@ -0,0 +1,11 @@
+r"""Contain factories for document stores."""
+
+from __future__ import annotations
+
+__all__ = ["BaseDocumentStoreFactory", "ConfigurableDocumentStoreFactory", "DocumentStoreFactory"]
+
+from zenpyre.document_stores.factory.base import BaseDocumentStoreFactory
+from zenpyre.document_stores.factory.configurable import (
+    ConfigurableDocumentStoreFactory,
+)
+from zenpyre.document_stores.factory.vanilla import DocumentStoreFactory
