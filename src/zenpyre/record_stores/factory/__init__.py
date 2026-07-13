72c744076c6545f9ac23a568155aa9aadeadef5a
Thibaut Durand <durand.tibo@gmail.com>
2026-07-13 11:44:34 -0700

    Add `DuckDBRecordStoreFactory` (#314)

--- src/zenpyre/record_stores/factory/__init__.py
+++ src/zenpyre/record_stores/factory/__init__.py
@@ -2,8 +2,14 @@ r"""Contain factories for record stores."""
 
 from __future__ import annotations
 
-__all__ = ["BaseRecordStoreFactory", "ConfigurableRecordStoreFactory", "RecordStoreFactory"]
+__all__ = [
+    "BaseRecordStoreFactory",
+    "ConfigurableRecordStoreFactory",
+    "DuckDBRecordStoreFactory",
+    "RecordStoreFactory",
+]
 
 from zenpyre.record_stores.factory.base import BaseRecordStoreFactory
 from zenpyre.record_stores.factory.configurable import ConfigurableRecordStoreFactory
+from zenpyre.record_stores.factory.duckdb import DuckDBRecordStoreFactory
 from zenpyre.record_stores.factory.vanilla import RecordStoreFactory
